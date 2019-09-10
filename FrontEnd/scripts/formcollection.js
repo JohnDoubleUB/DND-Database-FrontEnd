cc1f959abbdb4eda9b924100277d578718d408ca
John Brockman <johndoubleub@gmail.com>
2019-09-10 08:10:24 +0100

    Started adding functionality for create-edit-character and create-edit-inventory!

--- FrontEnd/scripts/formcollection.js
+++ FrontEnd/scripts/formcollection.js
@@ -1,24 +1,36 @@
+const url = new URLSearchParams(location.search);
+
 function submitCharacterForm(formData){
     let keyValues = {};
-    let skillprofs = {};
-    let savingthrows = {};
 
     for(let element of formData.elements){
-        //console.log(String(element.name));
-        //console.log(element.type == "checkbox"); // Only type of checkbox
-        if(element.name == "skillprofs"){
-            skillprofs[element.value] = element.checked;
-
-        } else if (element.name == "savingthrows") {
-            savingthrows[element.value] = element.checked;
-        } else if(element.name){
+        if(element.name){
             keyValues[element.name] = element.value;
         }
 
     }
     console.log(keyValues);
-    // console.log(savingthrows);
-    // console.log(skillprofs);
+
+    makeRequest("http://localhost:9000/characters", keyValues, type="POST")
+    .then((data)=>{
+        console.log("Create character worked!" + data)
+        let parsedData = JSON.parse(data);
+        // let playerId = document.getElementById("playerId")
+        // playerId.innerHTML = "";
+
+        // let defaultDrop = document.createElement("option");
+        // defaultDrop.innerText = "[Create a New Character]"
+        // defaultDrop.value="n";
+        // playerId.appendChild(defaultDrop);
+        
+
+        // buildDropdown("http://localhost:9000/characters");
+        window.location.href = "create-edit-character.html" + "?id=" + parsedData.id
+
+    })
+    .catch((data)=>{
+        console.log("Create character failed!" + data);
+    })
 
     return false;
 }
@@ -42,4 +54,36 @@ function checkV(box, resting){
     } else if(parseInt(box.value) > parseInt(box.max)) {
         box.value = parseInt(resting);
     }
+}
+
+function buildDropdown(dataLink){
+    let playerId = document.getElementById("playerId");
+    
+    makeRequest(dataLink)
+    .then((data)=>{
+        console.log("it worked!" + data);
+
+        let parsedData = JSON.parse(data)
+        for(key of parsedData){
+            // Create option
+            let dropDResult = document.createElement("option");
+            // Add name for option
+            dropDResult.innerText = key["name"];
+            dropDResult.value = key["id"];
+
+
+            paramId = url.get('id');
+            if(String(key["id"]) == String(paramId)){
+                dropDResult.selected="selected";
+            }
+
+            //Add to the existing dropdown
+            playerId.appendChild(dropDResult);
+
+        }
+
+    })
+    .catch((data)=>{
+        console.log("It failed!" + data);
+    })
 }
