7a8a3664e409b353b52532dc6d5dab4adb7bdfb6
johndoubleub <up741132@myport.ac.uk>
2019-09-11 17:30:52 +0100

    Started functionality for the Inventories page

--- FrontEnd/scripts/formcollection.js
+++ FrontEnd/scripts/formcollection.js
@@ -83,16 +83,68 @@ function setCharacterFields(newData){
 
 function submitInventoryForm(formData){
     let keyValues = {};
+    console.log("Inventory submission form!");
 
-    for(let element of formData.elements){
-        if(element.name){
-            keyValues[element.name] = element.value;
+    //Check that the user has selected a character
+    if(formData.playerId.value !== "n"){
+        console.log("An object!");
+        
+        for(let element of formData.elements){
+            if(element.name){
+                keyValues[element.name] = element.value;
+            }
         }
+        console.log(keyValues);
+
+        //Get current inventories
+        makeRequest("http://localhost:9000/inventories")
+        .then((data) => { 
+            let parsedData = JSON.parse(data); 
+            //Check if any for the given player id exist already
+            let idExists = false;
+
+            for(inventory in parsedData){
+                if(String(playerId.value) === (inventory.playerId)) {
+                    idExists = true;
+                    break;
+                }
+            }
+
+            //If the an inventory for this player id doesn't exist create one
+            if(!idExists){
+                makeRequest("http://localhost:9000/inventories", keyValues, type="POST")
+                .then((data) => {
+                    console.log("it worked!", data);
+                })
+                .catch((data) => {
+                    console.log("it failed!", data);
+                });
+            } else { //If an inventory does exist then update it!
+                makeRequest("http://localhost:9000/inventories", keyValues, type="PUT")
+                .then((data) => {
+                    console.log("Update Worked!", data);
+                })
+                .catch((data) => {
+                    console.log("it failed!", data);
+                });
+            }
+
+        })
+        .catch((data) => {
+            return false
+        });
+
+
     }
-    console.log(keyValues);
+
 
     return false;
 }
+
+
+
+
+
 
 function checkV(box, resting){
     if(parseInt(box.value) < parseInt(box.min)){
