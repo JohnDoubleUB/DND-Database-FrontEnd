40b1f5be15a388b7163585851c76b1c3d69cbbd5
John Brockman <johndoubleub@gmail.com>
2019-09-22 17:09:55 +0100

    Made changes to tablescripts and pages containing tables to make the fields read more nicely

--- FrontEnd/scripts/tablescripts.js
+++ FrontEnd/scripts/tablescripts.js
@@ -8,80 +8,144 @@ let dummyData = [
 ];
 
 
-function getItemValues(record){
+function getItemValues(record, excludeValues=false, groupData=false, replaceWith=false){
     let data = [];
+    let groupedData = "";
+    let groupedDataCount = 0;
+    
+    let matcher = replaceWith[1];
+    let keys = replaceWith[0];
+
     
     for(let item in record){
+        
+        if(replaceWith && item === matcher){
+            data.push(keys[record[item]])
+            continue;
+        }
+
+        //filter out grouped data
+        if(groupData && groupData[0].some(group => group === item)){
+            groupedData += item + ": " + record[item] + "\n";
+            groupedDataCount++;
+            if(groupedDataCount === groupData[0].length){
+                data.push(groupedData);
+            }
+            continue;
+        }
+        
+        if(excludeValues && excludeValues.some(exclude => exclude === item)){
+            continue;
+        }
+
         if(record.hasOwnProperty(item)){
             data.push(record[item])
         }
     }
-
     return data;
 }
 
-function getItemNames(record){
+function getItemNames(record, excludeValues=false, groupData=false){
     let data = [];
+    let dataGrouped = false;
 
     for(let item in record){
+        
+        //filter out grouped data
+        if(groupData && groupData[0].some(group => group === item)){
+            if(!dataGrouped){
+                data.push(groupData[1]);
+                dataGrouped = true;
+            }
+            continue;
+        }
+
+        if(excludeValues && excludeValues.some(exclude => exclude === item)){
+            continue;
+        }
+
         if(record.hasOwnProperty(item)){
             data.push(item);
         }
     }
-
     return data;
 }
 
+function loadTableData(head, body, dataLink, buttonFunction, identifier="", excludeValues=false, groupData=false, niceNames=false, enterValue=false){
+    let keyValues = [];
 
-function loadTableData(head, body, dataLink, buttonFunction, identifier="", excludeIdentifier=false){
     makeRequest(dataLink)
     .then((data) => {
-        console.log("It worked!" + data);
-        
-        //Parse data
-        let parsedData = JSON.parse(data);
-        
-        //Create arrays
-        let headers = [];
-        let tableData = [];
-        let recIds = [];
-
-
-        //Fill tableDataArray with all customerd data
-        for(let record of parsedData) {
-            let recordSet = getItemValues(record, identifier, excludeIdentifier); //Add an exclude argument to function as well as here!
+        if(enterValue){
             
-            recIds.push(record[identifier]);
-            tableData.push(recordSet);
+            makeRequest(enterValue[0]).then(data2 => {
+                let parsedData = JSON.parse(data2);
+                let keyValueSets = {};
+
+                for(let dataSet of parsedData){
+                    keyValueSets[dataSet[enterValue[1]]] = dataSet[enterValue[2]];
+                }
+
+                keyValues.push(keyValueSets);
+                keyValues.push(enterValue[3]);
+
+                loadTable(data, head, body, buttonFunction, identifier, excludeValues, groupData, niceNames, keyValues);
+            });
+        } else {
+            loadTable(data, head, body, buttonFunction, identifier, excludeValues, groupData, niceNames);
         }
-        
-        //Get headers for the table
-        headers = getItemNames(parsedData[0], identifier, excludeIdentifier); //Add an exclude argument to function as well as here!
-        
-        //Add option if there is any table entires
-        if(headers.length > 0) { headers.push("Option"); }
-
-        //Empty table head
-        head.innerHTML = "";
-
-        //Empty table body
-        body.innerHTML = "";
-
-        //Build Table head
-        buildTable(head, headers, true);
-        
-        
-        //Replace with iterative for loop
-        for(let i = 0; i < tableData.length; i++){
-            let dData = tableData[i];
-            let recId = i; //recIds[i]; //This way they are uniquely numbered but nicely!
-            buildTable(body, dData, false, buttonFunction, recId);            
-        }
-
-    })
-    .catch((data) => {
-        console.log("It failed!" + data);
     });
+}
+
+
+function loadTable(data, head, body, buttonFunction, identifier="", excludeValues=false, groupData=false, niceNames=false, keyValues=false){
+    console.log("It worked!" + data);
+
+    //Parse data
+    let parsedData = JSON.parse(data);
+
+    //Create arrays
+    let headers = [];
+    let tableData = [];
+    let recIds = [];
+
+
+    //Fill tableDataArray with all customerd data
+    for (let record of parsedData) {
+
+        let recordSet = getItemValues(record, excludeValues, groupData, keyValues); //Add an exclude argument to function as well as here!
+
+        recIds.push(record[identifier]);
+        tableData.push(recordSet);
+    }
+
+    //if using nice names, set the headers to be those instead!
+    if (niceNames) {
+        headers = niceNames;
+    } else {
+        //Get headers for the table
+        headers = getItemNames(parsedData[0], excludeValues, groupData, niceNames); //Add an exclude argument to function as well as here!
+    }
+
+    //Add option if there is any table entires
+    if (headers.length > 0) { headers.push("Option"); }
+
+    //Empty table head
+    head.innerHTML = "";
+
+    //Empty table body
+    body.innerHTML = "";
+
+    //Build Table head
+    buildTable(head, headers, true);
+
+
+    //Replace with iterative for loop
+    for (let i = 0; i < tableData.length; i++) {
+        let dData = tableData[i];
+        let recId = i; //recIds[i]; //This way they are uniquely numbered but nicely!
+        buildTable(body, dData, false, buttonFunction, recId);
+    }
 }
 
 
@@ -90,7 +154,7 @@ function deleteCharacterId(id){
     .then((data) => {
         parsedData = JSON.parse(data);
         makeRequest("http://"+DB_ADDRESS+":9000/inventories/playerid/",parsedData.id, type="DELETE");
-        loadTableData(tabHead, tabBod, "http://"+DB_ADDRESS+":9000/characters", "deleteCharacterId");
+        loadTableData(tabHead, tabBod, "http://"+DB_ADDRESS+":9000/characters", "deleteCharacterId","id", false, [["baseInt", "baseDex", "baseCon", "baseWis", "baseCha", "baseStr"], "Base Stats"], ["#", "Character Name","Race", "Class", "Alignment", "Background", "Level", "Base Stats", "HP", "Proficiency"]);
     })
     .catch((data) => {
         console.log("It failed!" + data);
@@ -102,7 +166,7 @@ function deleteCharacterId(id){
 function deleteInventoryId(id){
     makeRequest("http://"+DB_ADDRESS+":9000/inventories/", id, type="DELETE")
     .then((data) => {
-        loadTableData(tabHead, tabBod, "http://"+DB_ADDRESS+":9000/inventories/", "deleteInventoryId");
+        loadTableData(tabHead, tabBod, "http://"+DB_ADDRESS+":9000/inventories", "deleteInventoryId","playerId", false, false, ["#", "Owning Character", "CP", "SP", "GP", "PP", "Equipment"], ["http://"+DB_ADDRESS+":9000/characters", "id", "name", "playerId"]);
     })
     .catch((data) => {
         console.log("It failed!" + data);
@@ -162,4 +226,19 @@ function buildTableBody(container, tableData){ // To body
 
 function makeButton(functionName, functionArgument, buttonText, classStyling){
     return "<button class=\"" + classStyling + "\" onclick=\""+functionName+"('"+functionArgument+"')\""+">" + buttonText + "</button>";
+}
+
+function buildCharacterTable(){
+    loadTableData(tabHead, tabBod, 
+        "http://"+DB_ADDRESS+":9000/characters", 
+        "deleteCharacterId","id", false, 
+        [["baseInt", "baseDex", "baseCon", "baseWis", "baseCha", "baseStr"], "Base Stats"], 
+        ["#", "Character Name","Race", "Class", "Alignment", "Background", "Level", "Base Stats", "HP", "Proficiency"]);
+}
+
+function buildInventoryTable(){
+    loadTableData(tabHead, tabBod, 
+        "http://"+DB_ADDRESS+":9000/inventories", "deleteInventoryId","playerId", 
+        false, false, ["#", "Owning Character", "CP", "SP", "GP", "PP", "Equipment"], 
+        ["http://"+DB_ADDRESS+":9000/characters", "id", "name", "playerId"]);
 }
