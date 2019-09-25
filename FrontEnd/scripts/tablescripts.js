1624b8cd45faa1c15ea39b14252291f8adf8c8f1
John Brockman <johndoubleub@gmail.com>
2019-09-25 21:43:20 +0100

    heavily reduced complexity of tablescripts.js code

--- FrontEnd/scripts/tablescripts.js
+++ FrontEnd/scripts/tablescripts.js
@@ -1,176 +1,9 @@
 const tabHead = document.getElementById("tableHead");
 const tabBod = document.getElementById("tableBody");
 
-let dummyHeaders = ["Id", "Name", "Class", "Alignment", "Base Str", "Base Dex", "Base End", "Base Con", "Base Cha", "Base Wis", "Base Health", "Option1", "Option2"];
-let dummyData = [
-    ["1", "bob", "Fighter", "Chaotic Neutral", "10", "12", "14","17","12", "12", "5"],
-    ["2", "jeff", "Monk", "Chaotic Evil", "9", "12", "20","17","12", "12", "10"]
-];
 
-
-function getItemValues(record, excludeValues=false, groupData=false, replaceWith=false){
-    let data = [];
-    let groupedData = "";
-    let groupedDataCount = 0;
-    
-    let matcher = replaceWith[1];
-    let keys = replaceWith[0];
-
-    
-    for(let item in record){
-        
-        if(replaceWith && item === matcher){
-            data.push(keys[record[item]]);
-            continue;
-        }
-
-        //filter out grouped data
-        if(groupData && groupData[0].some(group => group === item)){
-            groupedData += item + ": " + record[item] + "\n";
-            groupedDataCount++;
-            if(groupedDataCount === groupData[0].length){
-                data.push(groupedData);
-            }
-            continue;
-        }
-        
-        if(excludeValues && excludeValues.some(exclude => exclude === item)){
-            continue;
-        }
-
-        if(record.hasOwnProperty(item)){
-            data.push(record[item]);
-        }
-    }
-    return data;
-}
-
-function getItemNames(record, excludeValues=false, groupData=false){
-    let data = [];
-    let dataGrouped = false;
-
-    for(let item in record){
-        
-        //filter out grouped data
-        if(groupData && groupData[0].some(group => group === item)){
-            if(!dataGrouped){
-                data.push(groupData[1]);
-                dataGrouped = true;
-            }
-            continue;
-        }
-
-        if(excludeValues && excludeValues.some(exclude => exclude === item)){
-            continue;
-        }
-
-        if(record.hasOwnProperty(item)){
-            data.push(item);
-        }
-    }
-    return data;
-}
-
-function loadTableData(head, body, dataLink, buttonFunction, identifier="", excludeValues=false, groupData=false, niceNames=false, enterValue=false){
-    let keyValues = [];
-
-    makeRequest(dataLink)
-    .then((data) => {
-        if(enterValue){
-            
-            makeRequest(enterValue[0]).then(data2 => {
-                let parsedData = JSON.parse(data2);
-                let keyValueSets = {};
-
-                for(let dataSet of parsedData){
-                    keyValueSets[dataSet[enterValue[1]]] = dataSet[enterValue[2]];
-                }
-
-                keyValues.push(keyValueSets);
-                keyValues.push(enterValue[3]);
-
-                loadTable(data, head, body, buttonFunction, identifier, excludeValues, groupData, niceNames, keyValues);
-            });
-        } else {
-            loadTable(data, head, body, buttonFunction, identifier, excludeValues, groupData, niceNames);
-        }
-    });
-}
-
-
-function loadTable(data, head, body, buttonFunction, identifier="", excludeValues=false, groupData=false, niceNames=false, keyValues=false){
-    console.log("It worked!" + data);
-
-    //Parse data
-    let parsedData = JSON.parse(data);
-
-    //Create arrays
-    let headers = [];
-    let tableData = [];
-    let recIds = [];
-
-
-    //Fill tableDataArray with all customerd data
-    for (let record of parsedData) {
-
-        let recordSet = getItemValues(record, excludeValues, groupData, keyValues); //Add an exclude argument to function as well as here!
-
-        recIds.push(record[identifier]);
-        tableData.push(recordSet);
-    }
-
-    //if using nice names, set the headers to be those instead!
-    if (niceNames) {
-        headers = niceNames;
-    } else {
-        //Get headers for the table
-        headers = getItemNames(parsedData[0], excludeValues, groupData, niceNames); //Add an exclude argument to function as well as here!
-    }
-
-    //Add option if there is any table entires
-    if (headers.length > 0) { headers.push("Option"); }
-
-    //Empty table head
-    head.innerHTML = "";
-
-    //Empty table body
-    body.innerHTML = "";
-
-    //Build Table head
-    buildTable(head, headers, true);
-
-
-    //Replace with iterative for loop
-    for (let i = 0; i < tableData.length; i++) {
-        let dData = tableData[i];
-        let recId = i; //recIds[i]; //This way they are uniquely numbered but nicely!
-        buildTable(body, dData, false, buttonFunction, recId);
-    }
-}
-
-
-function deleteCharacterId(id){
-    makeRequest("http://"+DB_ADDRESS+":9000/characters/", id, type="DELETE")
-    .then((data) => {
-        parsedData = JSON.parse(data);
-        makeRequest("http://"+DB_ADDRESS+":9000/inventories/playerid/",parsedData.id, type="DELETE");
-        loadTableData(tabHead, tabBod, "http://"+DB_ADDRESS+":9000/characters", "deleteCharacterId","id", false, [["baseInt", "baseDex", "baseCon", "baseWis", "baseCha", "baseStr"], "Base Stats"], ["#", "Character Name","Race", "Class", "Alignment", "Background", "Level", "Base Stats", "HP", "Proficiency"]);
-    })
-    .catch((data) => {
-        console.log("It failed!" + data);
-    })
-}
-
-
-
-function deleteInventoryId(id){
-    makeRequest("http://"+DB_ADDRESS+":9000/inventories/", id, type="DELETE")
-    .then((data) => {
-        loadTableData(tabHead, tabBod, "http://"+DB_ADDRESS+":9000/inventories", "deleteInventoryId","playerId", false, false, ["#", "Owning Character", "CP", "SP", "GP", "PP", "Equipment"], ["http://"+DB_ADDRESS+":9000/characters", "id", "name", "playerId"]);
-    })
-    .catch((data) => {
-        console.log("It failed!" + data);
-    });
+function makeButton(functionName, functionArgument, buttonText, classStyling){
+    return "<button class=\"" + classStyling + "\" onclick=\""+functionName+"('"+functionArgument+"')\""+">" + buttonText + "</button>";
 }
 
 
@@ -224,21 +57,92 @@ function buildTableBody(container, tableData){ // To body
     }
 }
 
-function makeButton(functionName, functionArgument, buttonText, classStyling){
-    return "<button class=\"" + classStyling + "\" onclick=\""+functionName+"('"+functionArgument+"')\""+">" + buttonText + "</button>";
-}
+
 
 function buildCharacterTable(){
-    loadTableData(tabHead, tabBod, 
-        "http://"+DB_ADDRESS+":9000/characters", 
-        "deleteCharacterId","id", false, 
-        [["baseInt", "baseDex", "baseCon", "baseWis", "baseCha", "baseStr"], "Base Stats"], 
-        ["#", "Character Name","Race", "Class", "Alignment", "Background", "Level", "Base Stats", "HP", "Proficiency"]);
+
+    let cTableHeaders = ["#", "Character Name", "Race", "Class", "Alignment", "Background", "Level", "Base Stats", "HP", "Proficiency", "Option"];
+    
+    tabHead.innerHTML = "";
+    tabBod.innerHTML = "";
+
+    buildTable(tabHead, cTableHeaders, true);
+
+    makeRequest("http://"+DB_ADDRESS+":9000/characters").then((data) => {
+        let parsedData = JSON.parse(data);
+        //let baseStatNames = [str];
+        let cTableBody = [];
+        let idCounter = 0;
+
+        for(let record of parsedData){
+
+            idCounter++;
+
+            cBaseStats = "Str: " + record.baseStr + "\n";
+            cBaseStats += "Int: " + record.baseInt + "\n";
+            cBaseStats += "Wis: " + record.baseWis + "\n";
+            cBaseStats += "Cha: " + record.baseCha + "\n";
+            cBaseStats += "Dex: " + record.baseDex + "\n";
+            cBaseStats += "Con: " + record.baseCon + "\n";
+
+            cTableBody = [record.id, record.name, record.race, record.playerClass, record.alignment, record.background, record.level, cBaseStats, record.baseHP, record.baseProficiency];
+
+            buildTable(tabBod, cTableBody, head=false, "deleteCharacterId", idCounter);
+        }
+    });
 }
 
+
 function buildInventoryTable(){
-    loadTableData(tabHead, tabBod, 
-        "http://"+DB_ADDRESS+":9000/inventories", "deleteInventoryId","playerId", 
-        false, false, ["#", "Owning Character", "CP", "SP", "GP", "PP", "Equipment"], 
-        ["http://"+DB_ADDRESS+":9000/characters", "id", "name", "playerId"]);
+    
+    let iTableHeaders = ["#", "Owning Character", "CP", "SP", "GP", "PP", "Equipment", "Option"];
+
+    tabHead.innerHTML = "";
+    tabBod.innerHTML = "";
+
+    buildTable(tabHead, iTableHeaders, true);
+
+    makeRequest("http://"+DB_ADDRESS+":9000/characters").then((characterData) => {
+        makeRequest("http://"+DB_ADDRESS+":9000/inventories").then((inventoryData) => {
+            let parsedCharData = JSON.parse(characterData);
+            let parsedInvData = JSON.parse(inventoryData);
+
+            let iTableBody = [];
+            let playerIdNames = {};
+
+            console.log(parsedCharData);
+            console.log(parsedInvData);
+
+            for(let record of parsedCharData){
+                playerIdNames[record.id] = record.name;
+            }
+
+            let idCounter = 0;
+
+            for(let record of parsedInvData){
+                idCounter++;
+                iTableBody = [record.id, playerIdNames[record.playerId], record.copperPiece, record.silverPiece, record.goldPiece, record.platinumPiece, record.equipment];
+                buildTable(tabBod, iTableBody, head=false, "deleteInventoryId", idCounter);
+            }
+        });
+    });
+}
+
+
+function deleteCharacterId(id){
+    makeRequest("http://"+DB_ADDRESS+":9000/characters/", id, type="DELETE")
+    .then((data) => {
+        parsedData = JSON.parse(data);
+        makeRequest("http://"+DB_ADDRESS+":9000/inventories/playerid/",parsedData.id, type="DELETE");
+        buildCharacterTable();
+    });
+}
+
+
+
+function deleteInventoryId(id){
+    makeRequest("http://"+DB_ADDRESS+":9000/inventories/", id, type="DELETE")
+    .then((data) => {
+        buildInventoryTable();
+    });
 }
